e85a16fc488b423c4bf7cbe82440169ba3edf84a
zaikunzhang <zaikunzhang@gmail.com>
2023-10-22 21:32:36 +0800

    revise test_copyfile.m

--- copyfile/test_copyfile.m
+++ copyfile/test_copyfile.m
@@ -1,6 +1,6 @@
 function test_copyfile()
 
-system('rm -rf folder*; mkdir folder1; cd folder1; touch a; touch b; touch c; ln -s b blink; rm b; cd ..');
+system('rm -rf folder*; mkdir folder1; cd folder1; touch a b c d e f g; ln -s d dlink; rm d; cd ..');
 
 success = copyfile('folder1','folder2');
 assert(success);
