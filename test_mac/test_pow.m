f37ce9d522e9d4059ccfdf8cfbbb8455506ab201
Zaikun ZHANG <zaikunzhang@gmail.com>
2024-02-16 19:28:29 +0800

    Update test_pow.m

--- test_mac/test_pow.m
+++ test_mac/test_pow.m
@@ -2,6 +2,6 @@ function y = test_pow(x)
 
 mex('-setup', '-v', 'Fortran');
 mex('-v', 'pow.F');
-mex -v LDFLAGSVER='-Wl,-twolevel_namespace -Wl,-undefined,dynamic_link -mmacosx-version-min=$MACOSX_DEPLOYMENT_TARGET -Wl,-syslibroot,$ISYSROOT $LINKEXPORTVER $BUNDLEFLAG' pow.F
+mex -v LDFLAGSVER='-Wl,-twolevel_namespace -Wl,-undefined,dynamic_lookup -mmacosx-version-min=$MACOSX_DEPLOYMENT_TARGET -Wl,-syslibroot,$ISYSROOT $LINKEXPORTVER $BUNDLEFLAG' pow.F
 y = pow(x);
 %assert(y == x^int64(x), 'pow failed')
