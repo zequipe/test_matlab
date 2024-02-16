4ff7807d464f7addc86b4ab67ec5bcc50ba73bc5
Zaikun ZHANG <zaikunzhang@gmail.com>
2024-02-16 22:35:10 +0800

    Update test_pow.m

--- test_mac/test_pow.m
+++ test_mac/test_pow.m
@@ -1,8 +1,9 @@
 function y = test_pow(x)
 
-mex('-setup', '-v', 'Fortran');
-getenv('LDFLAGSVER');
+mex('-setup', 'Fortran');
+getenv('LDFLAGSVER')
 setenv('LDFLAGSVER', '-Wl,-undefined,dynamic_lookup');
+getenv('LDFLAGSVER')
 mex('-v', 'pow.F');
 y = pow(x);
 %assert(y == x^int64(x), 'pow failed')
