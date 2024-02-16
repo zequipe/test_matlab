157bc8b287d38e85560d0af3964b8462ba27e96b
Zaikun ZHANG <zaikunzhang@gmail.com>
2024-02-17 00:26:47 +0800

    Update test_pow.m

--- test_mac/test_pow.m
+++ test_mac/test_pow.m
@@ -1,8 +1,8 @@
 function y = test_pow(x)
 
 mex('-setup', 'Fortran');
-%linker_options = 'LDFLAGSVER=$(echo $LDFLAGSVER | sed "s/-undefined error/-undefined dynamic_lookup/g")';
-linker_options = 'LINKFLAGS="$LINKFLAGS -Wl,-undefined,dynamic_lookup"';
+linker_options = 'LDFLAGSVER=$(echo $LDFLAGSVER | sed "s/-undefined error/-undefined dynamic_lookup/g")';
+%linker_options = 'LINKFLAGS="$LINKFLAGS -Wl,-undefined,dynamic_lookup"'; % This does not work
 mex('-v', linker_options, 'pow.F');
 y = pow(x);
 %assert(y == x^int64(x), 'pow failed')
