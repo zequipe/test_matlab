33ba97dc1a56635495c2242442e30f6853ff645c
Zaikun ZHANG <zaikunzhang@gmail.com>
2024-02-17 01:56:31 +0800

    Update test_pow.m

--- test_mac/test_pow.m
+++ test_mac/test_pow.m
@@ -1,10 +1,15 @@
 function y = test_pow(x)
 
 mex('-setup', 'Fortran');
-%linker_options = 'LDFLAGSVER=$(echo $LDFLAGSVER | sed "s/-undefined error/-undefined dynamic_lookup/g")'; % This fixes the failure
+
+% The following works without any issue.
 linker_options = 'LDFLAGSVER=$(echo $LDFLAGSVER | sed "s/-undefined error//g") -undefined dynamic_lookup'; % This fixes the failure
 %linker_options = 'LINKFLAGS="$LINKFLAGS -Wl,-undefined,dynamic_lookup"'; % This does not work
 mex('-v', linker_options, 'pow.F');
-%mex('-v', 'pow.F');
 y = pow(x);
-%assert(y == x^int64(x), 'pow failed')
+assert(y == x^int64(x), 'pow failed');
+
+% The following fails due to undefined symbols. 
+mex('-v', 'pow.F');
+y = pow(x);
+assert(y == x^int64(x), 'pow failed');
