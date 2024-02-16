339fefc2b720c25ae97c32b3bcabdedb8318145f
Zaikun ZHANG <zaikunzhang@gmail.com>
2024-02-17 03:08:58 +0800

    Update test_pow.m

--- test_mac/test_pow.m
+++ test_mac/test_pow.m
@@ -4,7 +4,7 @@ mex('-setup', 'Fortran');
 
 % The following works without any issue.
 %linker_options = 'LDFLAGSVER=$(echo $LDFLAGSVER | sed "s/-undefined error//g") -undefined dynamic_lookup'; % This fixes the failure
-%linker_options = 'LDFLAGSVER="$LDFLAGSVER -undefined dynamic_lookup"'; 
+linker_options = 'LDFLAGSVER="$LDFLAGSVER -undefined dynamic_lookup"'; 
 %linker_options = 'LINKFLAGS="$LINKFLAGS -Wl,-undefined,dynamic_lookup"'; % This does not work
 mex('-v', linker_options, 'pow.F');
 y = pow(x)
