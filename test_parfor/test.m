1a9be451a2060527509192f923f007be37b096b8
zaikunzhang <zaikunzhang@gmail.com>
2025-09-18 13:48:40 +0800

    add test_parfor

---
+++ test_parfor/test.m
@@ -0,0 +1,20 @@
+function test()
+
+    olddir = pwd();
+    cd(tempdir);
+    fprintf('\nCurrent directory: %s\n', pwd());
+
+    func = @(x) fun(x, 0);
+
+    addpath(fullfile(fileparts(mfilename('fullpath')), 'try_parfor'));
+    try_parfor(func);
+
+    cd(olddir);
+    fprintf('\nCurrently directory: %s\n', pwd());
+
+end
+
+
+function y = fun(x, z)
+    y = x + z;
+end
---
+++ test_parfor/try_parfor/try_parfor.m
@@ -0,0 +1,12 @@
+function try_parfor(func)
+
+    fprintf('\nEntering try_parfor\n');
+    fprintf('\nCurrently directory: %s\n', pwd());
+
+    parfor i = 1:2
+        fprintf("\n%d\n", feval(func, i));
+    end
+
+    fprintf('\nExiting try_parfor\n');
+
+end
